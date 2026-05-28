9b5a7849938b9d314fbe5916b4e408fb3dca39db
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-05-28 13:44:58 +0200

    todolist (CLI) - start

--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
-﻿// get input from user, then reply with something
-//Console.WriteLine("Hi, please enter your name");
-
-
-// in js variable only needs let/const, name and value
-// ie: let variable = "test";
-
-// decleare variable, needs 3 things:
-// type, name and value
-// ie:
-string name = "Patryk";
-
-// Console.WriteLine(name);
-
-//Console.ReadLine();
+﻿// Console based todolist app (CLI-app) Command Line Interface
